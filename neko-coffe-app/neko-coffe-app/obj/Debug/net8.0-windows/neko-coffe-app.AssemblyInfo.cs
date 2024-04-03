8bb22790eb0cb9578d0a68ceaa80c159d951ff94
dducktai <dangductai6410@gmail.com>
2024-04-03 07:37:33 +0700

    logo

--- neko-coffe-app/neko-coffe-app/obj/Debug/net8.0-windows/neko-coffe-app.AssemblyInfo.cs
+++ neko-coffe-app/neko-coffe-app/obj/Debug/net8.0-windows/neko-coffe-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("neko-coffe-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f807c9a2c96a23b3f4ef1603636be935f8ed96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff6dfcab22c8c8858b02c44f9b5a0f302189448")]
 [assembly: System.Reflection.AssemblyProductAttribute("neko-coffe-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("neko-coffe-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
