e387d83e1029c8bdce7d6d62ced5259b39cd5613
hohuyy <108950770+hohuyy@users.noreply.github.com>
2024-05-10 21:39:41 +0700

    Them user control AdminMenu

---
+++ NekoCoffeApp/AdminMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class AdminMenu : UserControl
+    {
+            private static AdminMenu _instance;
+            public static AdminMenu Instance
+            {
+                get
+                {
+                    if (_instance == null)
+                        _instance = new AdminMenu();
+                    return _instance;
+                }
+            }
+            public AdminMenu()
+            {
+                InitializeComponent();
+            }
+
+            private void AdminMenu_Load (object sender, EventArgs e)
+            {
+
+            }
+    }
+}
