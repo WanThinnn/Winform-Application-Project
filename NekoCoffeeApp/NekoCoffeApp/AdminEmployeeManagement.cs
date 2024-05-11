3ce9539457f9f18585528d4255e0e07f158dc21c
hohuyy <sincohuy51@gmail.com>
2024-05-11 14:18:23 +0700

    Tao class NekoEmployee

--- NekoCoffeeApp/NekoCoffeApp/AdminEmployeeManagement.cs
+++ NekoCoffeeApp/NekoCoffeApp/AdminEmployeeManagement.cs
@@ -23,30 +23,44 @@ namespace UI
             InitializeComponent();
         }
 
+        IFirebaseConfig ifc = new FirebaseConfig()
+        {
+            AuthSecret = "f5A5LselW6L4lKJHpNGVH6NZHGKIZilErMoUOoLC",
+            BasePath = "https://neko-coffe-database-default-rtdb.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+        private void Signup_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(ifc);
+            }
+
+            catch
+            {
+                MessageBox.Show("Kiểm tra lại mạng", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
         private void AdminAddEmployee_Click(object sender, EventArgs e)
         {
-            //IFirebaseConfig ifc = new FirebaseConfig()
-            //{
-            //    AuthSecret = "f5A5LselW6L4lKJHpNGVH6NZHGKIZilErMoUOoLC",
-            //    BasePath = "https://neko-coffe-database-default-rtdb.firebaseio.com/"
-            //};
+           se res = client.Get(@"Users/" + AdminFillEmployeeUserName.text);
+            NekoUser ResUser = res.ResultAs<NekoUser>();
 
-            //onse res = client.Get(@"Users/" + AdminFillEmployeeUserName.Text);
-            //NekoUser ResUser = res.ResultAs<NekoUser>();
+            NekoUser employee = new NekoUser()
+            {
+                EmployeeName = AdminFillEmployeeName.text,
+                Password = "",
+                DateOfBirth = AdminFillEmployeeDateOfBirth.text,
+                Gender = AdminFillEmployeeGender.text,
+                Address = AdminFillEmployeeAddress.text,
+                PhoneNumber = AdminFillEmployeePhoneNumber.text,
+                Email = AdminFillEmployeeEmail.text,
+                Salary = AdminFillEmployeeSalary.text
+            };
 
-            //NekoUser employee = new NekoUser()
-            //{
-            //    EmployeeName = AdminFillEmployeeName.Text,
-            //    Password = ""
-            //    DateOfBirth = AdminFillEmployeeDateOfBirth.Text,
-            //    Gender = AdminFillEmployeeGender.Text,
-            //    Address = AdminFillEmployeeAddress.Text,
-            //    PhoneNumber = AdminFillEmployeePhoneNumber.Text,
-            //    Email = AdminFillEmployeeUserName.Text,
-            //    Salary = AdminFillEmployeeSalary.Text
-            //};
-
-            //SetResponse set = client.Set(@"Users/" + txtUserName.Text, user);
+            setresponse set = client.set(@"users/" + txtusername.text, user);
 
         }
     }
---
+++ NekoCoffeeApp/NekoCoffeApp/NekoEmployee.cs
@@ -0,0 +1,78 @@
+﻿using BCrypt.Net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    internal class Nekoemployee
+    {
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Salary { get; set; }
+        public string UserName { get; set; }
+
+
+        private static string error1 = "Không tồn tại!";
+        private static string error2 = "Cảnh báo";
+
+        public static void ShowError()
+        {
+
+            System.Windows.Forms.MessageBox.Show(error1, error2, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        public static void ShowError_2()
+        {
+            System.Windows.Forms.MessageBox.Show("Không tồn tại!", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        public static bool IsEqual(NekoEmployee employee1, NekoEmployee employee2)
+        {
+            if (employee1 == null || employee2== null) { return false; }
+
+            if (employee1.Name != employee2.Name)
+            {
+                error1 = "Nhân viên không tồn tại!";
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsExist(NekoEmployee employee1, NekoEmployee employee2)
+        {
+            if (employee1 == null || employee2== null) { return false; }
+
+            if (employee1.Username != employee2.Username || employee1.Email != employee2.Email || employee1.PhoneNumber != employee2.PhoneNumber)
+            {
+                error1 = "Tài khoản không tồn tại!";
+                return false;
+            }
+
+
+            return true;
+        }
+
+
+        public static bool Search(NekoEmployee employee1, NekoEmployee employee2)
+        {
+            if (employee1 == null || employee2== null) { return false; }
+
+            if (employee1.Username != employee2.Username)
+            {
+                System.Windows.Forms.MessageBox.Show("Tài khoản không tồn tại!", "Cảnh báo!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
