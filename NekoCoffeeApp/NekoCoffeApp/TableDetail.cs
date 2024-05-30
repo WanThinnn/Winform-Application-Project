ee5d6c5c6131a68c30f30a2ee68955ca46a567c6
Nam Phuong <baymax2501@gmail.com>
2024-05-30 14:25:57 +0700

    set up firebase cho tablesdetail

--- NekoCoffeeApp/NekoCoffeApp/TableDetail.cs
+++ NekoCoffeeApp/NekoCoffeApp/TableDetail.cs
@@ -15,6 +15,12 @@ namespace UI
 {
     public partial class TableDetail : Form
     {
+        DataTable mydt = new DataTable();
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "f5A5LselW6L4lKJHpNGVH6NZHGKIZilErMoUOoLC",
+            BasePath = "https://neko-coffe-database-default-rtdb.firebaseio.com/"
+        };
         public TableDetail() {}
         private NekoTable _table;
         public TableDetail(NekoTable table)
@@ -23,26 +29,12 @@ namespace UI
             _table = table;
             LoadTableDetails();
         }
-
         private void LoadTableDetails()
         {
            
         }
 
 
-
-
-
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void bunifuDataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
 
         private void TableDetail_Load(object sender, EventArgs e)
         {
